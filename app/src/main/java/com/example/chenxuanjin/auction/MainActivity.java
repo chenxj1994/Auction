b4fab1b65586fb2d587bf93cd6f9587a62a5865a
chenxuanjin <420803276@qq.com>
2017-04-03 20:29:27 +0800

    主题框架上传

---
+++ app/src/main/java/com/example/chenxuanjin/auction/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.chenxuanjin.auction;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+import com.example.chenxuanjin.auction.fragment.HomeFragment;
+import com.example.chenxuanjin.auction.fragment.MeFragment;
+import com.example.chenxuanjin.auction.fragment.PublishFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Fragment homeFragment ,meFragment ,publishFragment;
+    private FragmentTransaction transaction;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        //BottomNavigation监听器
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            transaction = getFragmentManager().beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    if(homeFragment == null){
+                        homeFragment = new HomeFragment();
+                    }
+                    transaction.replace(R.id.content,homeFragment);
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    if(publishFragment == null){
+                        publishFragment = new PublishFragment();
+                    }
+                    transaction.replace(R.id.content,publishFragment);
+                    transaction.commit();
+                    return true;
+                case R.id.navigation_notifications:
+                    if(meFragment == null){
+                        meFragment = new MeFragment();
+                    }
+                    transaction.replace(R.id.content,meFragment);
+                    transaction.commit();
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setDefaultFragment();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    /**
+     * 设置默认的Fragment
+     */
+    private void setDefaultFragment(){
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction transaction= fm.beginTransaction();
+        homeFragment = new HomeFragment();
+        transaction.replace(R.id.content,homeFragment);
+        transaction.commit();
+    }
+
+}
